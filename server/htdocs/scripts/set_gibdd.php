1509158c0090d156b1257bda98c82b0976272a74
RussianFox <horonitel@gmail.com>
2020-03-29 10:57:22 +0300

    Fix gibdd script

--- server/htdocs/scripts/set_gibdd.php
+++ server/htdocs/scripts/set_gibdd.php
@@ -11,13 +11,15 @@ function numberFormat($digit, $width) {
 echo "Start ".date('Y-m-d H:i:s')."\r\n";
 
 $ex_regs=array(20,80,81,84,85,88,90,91,93,94);
-$index="speedcammapping_gibdd";
-
-$params = ['index' => $index];
-$bool=$client->indices()->exists($params);
+$index="roadsituation_gibdd";
+$index_alias=FALSE; //FALSE if no need
+$bool=$client->indices()->exists(['index' => $index]);
 if (!$bool) {
-    $response = $client->indices()->create($params);
+	echo "Index is not exist, creating it \r\n";
+    $response = $client->indices()->create(['index' => $index]);
 }
+$query = $client->count(['index' => $index]);
+$docsCount_start=1*$query['count'];
 
 $need_clean=true;
 $ids=array();
@@ -26,7 +28,7 @@ for ($ii=1;$ii<96;$ii++) {
     if (in_array($ii,$ex_regs)) { continue; };
 
     $url = "https://xn--90adear.xn--p1ai/r/".numberFormat($ii, 2)."/milestones/";
-    echo "Region $ii\n";
+    echo "Region $ii \r\n";
 
     $file=false;
     $need_continue=5;
@@ -44,34 +46,46 @@ for ($ii=1;$ii<96;$ii++) {
     $i=0;
     $matches=false;
     if ($file) {
-	preg_match_all('/balloonContentBody.*\/milestones\/(.*)\/.*balloonContentHeader: "(.*)".*coordinates:.*\[(.*), (.*)\]/sUsi',$file, $matches);
-        //print_r($matches);
+		preg_match_all('/balloonContentBody.*\/milestones\/(.*)\/.*balloonContentHeader: "(.*)".*coordinates:.*\[(.*), (.*)\]/sUsi',$file, $matches);
 
-	for ($i=0; $i<count($matches[1]); $i++) {
-	    $id = $matches[1][$i];
-	    $link="https://гибдд.рф/milestones/".$matches[1][$i]."/";
-	    $name=trim(str_replace('&quot;', '"', $matches[2][$i]));
-	    $lat=1*trim($matches[3][$i]);
-	    $lng=1*trim($matches[4][$i]);
-	    //echo "$lat|$lng $name ($link)\n";
-	    //echo "$lng\n";
-	    if (!update_object_int($id,$lng,$lat,"cam",$name,null,$link,$index)) {
-		echo "Failed: $lat|$lng $name ($link)\n";
-	    };
-	    $ids[]=$id;
-	};
-	echo "Region loaded success: $i\n";
+		for ($i=0; $i<count($matches[1]); $i++) {
+			$id = $matches[1][$i];
+			$link="https://гибдд.рф/milestones/".$matches[1][$i]."/";
+			$name="Регион ".$ii.". ".trim(str_replace('&quot;', '"', $matches[2][$i]));
+			$lat=1*trim($matches[3][$i]);
+			$lng=1*trim($matches[4][$i]);
+			if (!update_object_int($id,$lng,$lat,"cam",$name,null,$link,$index)) {
+				echo "Failed: $lat|$lng $name ($link) \r\n";
+			};
+			$ids[]=$id;
+		};
+		echo "Region ".$ii." loaded success: $i \r\n";
     } else {
-	echo "Region loading failed\n";
-	$need_clean=false;
+		echo "Region ".$ii." loading failed \r\n";
+		$need_clean=false;
     }
 }
 
+echo "Loading objects success \r\n";
+
+$query = $client->count(['index' => $index]);
+$docsCount_add=1*$query['count'];
+$docsCount_clean=$docsCount_add;
 if ($need_clean) {
-    echo "Starting cleaning...\n";
-    clean_objects($index,'must_not',$ids);
+	if (((count($ids))/$docsCount_start)*100 > 70) {
+		clean_objects($index,'must_not',$ids);
+		echo "Cleaning success \r\n";
+		$query = $client->count(['index' => $index]);
+		$docsCount_clean=1*$query['count'];
+	} else {
+		echo "Cleaning cancelled \r\n";
+	};
+} else {
+	echo "Cleaning canceled, we have broken regions \r\n";
 };
 
+if ($index_alias) { replace_index_alias($index,$index_alias); };
+echo "Statistics. Docs added: ".($docsCount_add-$docsCount_start)." Docs cleaned: ".($docsCount_add-$docsCount_clean)." Docs now: ".$docsCount_clean."\r\n";
 echo "Finish ".date('Y-m-d H:i:s')."\r\n";
 echo "----------------------------------------\r\n";
 ?>
